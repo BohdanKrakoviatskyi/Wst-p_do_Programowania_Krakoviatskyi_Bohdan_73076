4133bc3db220f645fb9ca1c10403c79ca6cf95c5
73076 <73076@student.wsiz.edu.pl>
2025-10-28 12:40:30 +0100

    zadanie zrobione na lekcji

--- lab1/zad6.py
+++ lab1/zad6.py
@@ -1,6 +1,6 @@
 a = float(input("Podaj długość drogi :"))
 b = float(input("Ile samochód mają średnie spalamje (w litrach na 100km):"))
 cena = 6.5
-zuzycie = a/b
+zuzycie = a*(b/100)
 print(f"{zuzycie} litrów")
 print(f"{zuzycie*cena} zł")
