9e5e09a0fa3942becee2c067e47bab24aa7f6ff1
73076 <73076@student.wsiz.edu.pl>
2025-10-28 11:50:52 +0100

    zadanie 3 Uzupełnił w kodzie zmienne imie, kierunek, uczelnia swoimi danymi, a następnie wyświetl je w formie pełnych zdań.

---
+++ lab1/lab1.py
@@ -0,0 +1,8 @@
+imie = 'Bohdan'
+kierunnek = 'cyberbiezpieczenstwo'
+uczielnia = 'Wsiz'
+
+print('nazywam sie' , imie, 'studiuje kierunek' , f'{kierunnek},' )
+#to rachuje ile ja mam lat
+S2 = (10+7)
+print(f'Ja mam {S2}', 'lat' ,'uczy sie w uczielni' , uczielnia,)
