bb58309aa5633a4f0a492ae25a21aabdefffddf0
Smart <73076@student.wsiz.edu.pl>
2025-11-03 17:54:35 +0100

    zadanie domowe L1

---
+++ lab1/zad10.py
@@ -0,0 +1,12 @@
+x = float(input("Podaj pierwszą liczbę (x): "))
+y = float(input("Podaj drugą liczbę (y): "))
+z = float(input("Podaj trzecią liczbę (z): "))
+
+if x > y:
+    x, y = y, x
+if x > z:
+    x, z = z, x
+if y > z:
+    y, z = z, y
+
+print(f"Liczby w kolejności rosnącej: {x}, {y}, {z}")
