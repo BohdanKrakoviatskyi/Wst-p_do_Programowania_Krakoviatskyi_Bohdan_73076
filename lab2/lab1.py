ea1dddc8ceca9b9a778b6669943cf1669c7e34d8
Smart <73076@student.wsiz.edu.pl>
2025-12-09 20:13:09 +0100

    zadanie domowe l4

--- lab2/lab1.py
+++ lab2/lab1.py
@@ -1,18 +1,18 @@
-#ver1
-#for i in range(100):
-#    print(i)
-#1,2,3,...,99,100
+ver1
+for i in range(100):
+   print(i)
+1,2,3,...,99,100
 
-#ver2 A
-#for i in range(4, 7):
-#    print(i)
-#4,5,6 (7 niema)
+ver2 A
+for i in range(4, 7):
+   print(i)
+4,5,6 (7 niema)
 
-#ver3 B
-#for i in range(100, -1, -1):
-#    print(i)
-#100,99,...,2,1,0
+ver3 B
+for i in range(100, -1, -1):
+   print(i)
+100,99,...,2,1,0
 
-#C
-#for i in range(7, 77, 7):
-#    print(i)
+C
+for i in range(7, 77, 7):
+   print(i)
